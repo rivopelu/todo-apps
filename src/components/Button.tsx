0ff19a5985bd3849fb78bdeb65c9fe937aa912d9
rivopelu <contactrivopelu@gmail.com>
2022-11-30 19:39:05 +0700

    fix-commit-for-test-2

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -16,18 +16,18 @@ interface IBtn {
 export const Button = ({children, className, onClick, type, variant, dataCy}: IBtn) => {
   if (variant === BtnVariant.GRAY) {
     return (
-      <button type={type ?? "button"} onClick={onClick}
+      <button data-cy={dataCy} type={type ?? "button"} onClick={onClick}
               className={`bg-grayLight px-7 py-3 rounded-full  hover:bg-slate-200 duration-200 active:bg-slate-100 text-darkBlack font-bold ${className}`}>{children}</button>
     )
   } else if (variant === BtnVariant.RED) {
 
     return (
-      <button type={type ?? "button"} onClick={onClick}
+      <button data-cy={dataCy} type={type ?? "button"} onClick={onClick}
               className={`bg-mainRed px-7 py-3 rounded-full text-white hover:bg-red-600 duration-200 active:bg-red1-400 font-bold ${className}`}>{children}</button>
     )
   } else {
     return (
-      <button type={type ?? "button"} onClick={onClick}
+      <button data-cy={dataCy} type={type ?? "button"} onClick={onClick}
               className={`bg-primary px-7 py-3 rounded-full text-white hover:bg-sky-500 duration-200 active:bg-sky-400 font-bold ${className}`}>{children}</button>
     )
   }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export const Header = () => {
   return (
     <div data-cy="header-background" className={"bg-primary h-[105px] w-full z-[5000] fixed top-0 right-0"}>
       <MainContainer className={"h-full flex items-center"}>
-        <h1 data-cy="header-title" className={"text-3xl font-bold text-white uppercase"}>To Do List App</h1>
+        <div data-cy="header-title" className={"text-3xl font-bold text-white uppercase"}>To Do List App</div>
       </MainContainer>
     </div>
   )
